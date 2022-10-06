be815df2e6e083c87764c1a063804c5cb3c30d3d
naga <toritonsuibu0527@gmail.com>
2022-10-07 03:05:41 +0900

    init install for PC on Robot in 2022

---
+++ install.sh
@@ -0,0 +1,23 @@
+# install ros and so on
+sudo apt update
+sudo apt install gcc g++ g++-10 python3-pip net-tools cmake make openssh-server can-utils clang vim unar libasound2-dev
+
+pip install simpleaudio
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update
+sudo apt install ros-noetic-ros-base
+
+sudo apt install python3-rosdep
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+source /opt/ros/noetic/setup.bash
+
+sudo apt -y install python3-rosinstall
+sudo apt install -y python3-catkin-tools 
